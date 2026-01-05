a226606dade912557a1dde8c82e9a7d5125f43ec
grinfinity007 <shreyanshk315@gmail.com>
2026-01-06 02:47:40 +0530

    feat: implement auth service with PostgreSQL integration and Docker setup

---
+++ auth-service/src/db/db.js
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+
+const pool = Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+export default pool
+
