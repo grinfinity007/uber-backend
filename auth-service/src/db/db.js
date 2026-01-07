2aabfec604110af2df79cba6ad6889fdfe66d376
grinfinity007 <shreyanshk315@gmail.com>
2026-01-08 01:32:26 +0530

    fix: minor bugs

--- auth-service/src/db/db.js
+++ auth-service/src/db/db.js
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const pool = Pool({
+const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
--- auth-service/src/index.js
+++ auth-service/src/index.js
@@ -1,7 +1,6 @@
-import dotenv from "dotenv";
-import app from "./app.js";
+import "dotenv/config";
 
-dotenv.config();
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3001;
 
